72ac117437d42e0ff0c8cce2250435b5054bed6a
John Giatropoulos <john.giatropoulos@gmail.com>
2020-03-08 16:57:12 -0400

    Updated some styles

--- src/App.js
+++ src/App.js
@@ -6,10 +6,9 @@ import {
   SearchBox,
   Pagination,
   RefinementList,
-  RangeSlider,
   Stats,
 } from 'react-instantsearch-dom';
-import PropTypes from 'prop-types';
+import Hit from './Hit';
 import './App.css';
 
 const searchClient = algoliasearch(
@@ -41,8 +40,12 @@ class App extends Component {
                       placeholder: '',
                     }}
                   />
-                  <Stats />
-                  <Hits hitComponent={Hit} />
+                  <div className="statsbox">
+                    <Stats />
+                  </div>
+                  <div className="hitsbox">
+                    <Hits hitComponent={Hit} />
+                  </div>
 
                   <div className="pagination">
                     <Pagination />
@@ -56,32 +59,5 @@ class App extends Component {
     );
   }
 }
-
-function Hit(props) {
-  const description =
-    props.hit.description.length > 100
-      ? `${props.hit.description.substr(0, 100)}...`
-      : props.hit.description;
-  return (
-    <div className="hitTile">
-      <div className="hitImage">
-        <img
-          src={props.hit.image_url}
-          style={{ flex: 0, height: 120, width: undefined }}
-          alt={props.hit.name}
-        />
-      </div>
-      <h3>
-        <strong>{props.hit.name}</strong>
-      </h3>
-      <p>{description}</p>
-      <p className="tiny">Viewed {props.hit.viewed_count} times</p>
-    </div>
-  );
-}
-
-Hit.propTypes = {
-  hit: PropTypes.object.isRequired,
-};
 
 export default App;
---
+++ src/Hit.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Hit.css';
+
+class Hit extends React.Component {
+  handleClick = () => {};
+  render() {
+    const { hit } = this.props;
+    const speakers = hit.speakers.length > 0 ? hit.speakers.join(', ') : null;
+    const description =
+      hit.description.length > 100
+        ? `${hit.description.substr(0, 100)}...`
+        : hit.description;
+    return (
+      <div className="hitTile" onClick={this.handleClick}>
+        <div className="hitImage">
+          <img
+            src={hit.image_url}
+            style={{ flex: 0, height: 120, width: undefined }}
+            alt={hit.name}
+          />
+        </div>
+        <h3>
+          <strong>{hit.name}</strong>
+        </h3>
+        <p>{description}</p>
+        <p>
+          <span className="featuring">featuring</span>
+          <br />
+          <strong>{speakers}</strong>
+        </p>
+        <p className="tiny">{hit.viewed_count} views</p>
+      </div>
+    );
+  }
+}
+
+Hit.propTypes = {
+  hit: PropTypes.object.isRequired,
+};
+
+export default Hit;
