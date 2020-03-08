fbca23c830c4e9cd1d82a6e760b55ce534a21935
John Giatropoulos <john.giatropoulos@gmail.com>
2020-03-08 16:06:09 -0400

    Updates

---
+++ precache-manifest.a526096495c7171ad7e061d22e6c8b1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bddc851dc057cf2af151",
+    "url": "/tt-ui/static/js/runtime~main.bddc851d.js"
+  },
+  {
+    "revision": "559ed9af38aa0e4c568b",
+    "url": "/tt-ui/static/js/main.559ed9af.chunk.js"
+  },
+  {
+    "revision": "79c0b4d8cced0ff120d7",
+    "url": "/tt-ui/static/js/1.79c0b4d8.chunk.js"
+  },
+  {
+    "revision": "559ed9af38aa0e4c568b",
+    "url": "/tt-ui/static/css/main.ef1e9fa4.chunk.css"
+  },
+  {
+    "revision": "666ffd908e721d7affaa10908bafa711",
+    "url": "/tt-ui/index.html"
+  }
+];
